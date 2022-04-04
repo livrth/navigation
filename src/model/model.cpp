0c8f5689a96e0147f573972698a77075b0ae9d3d
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-04-04 12:05:17 +0800

    Add model setter and getter

---
+++ src/model/model.cpp
@@ -0,0 +1,21 @@
+//
+// Created by x on 4/4/0004.
+//
+
+#include "model.h"
+using std::shared_ptr, std::vector, std::string;
+
+Model::Model(string name) : name{name} {
+}
+void Model::set_id(Id id) {
+	this->id = id;
+}
+Id Model::get_id() {
+	return this->id;
+}
+void Model::set_name(string name) {
+	this->name = name;
+}
+string Model::get_name() {
+	return this->name;
+}
