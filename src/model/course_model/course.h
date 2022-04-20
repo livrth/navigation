e85321360c71722a11cea9bddcec765549309c00
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-04-20 21:26:24 +0800

    fix

--- src/model/course_model/course.h
+++ src/model/course_model/course.h
@@ -1,4 +1,18 @@
 #ifndef NAVIGATION_COURSE_H
 #define NAVIGATION_COURSE_H
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Course {
+    public:
+    string course_id; //课程编号
+    string course_name; //课程名称
+    
+    Course();
+    Course(string course_id, string course_name);
+};
+
 #endif  // NAVIGATION_COURSE_H
