770f6b7fb0c604890b512ac14b0df869e5e11342
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-04-20 19:13:20 +0800

    fix cmake version problem

--- src/model/building_model/building.h
+++ src/model/building_model/building.h
@@ -2,28 +2,27 @@
 #define NAVIGATION_BUILDING_H
 
 #include <memory>
-#include <vector>
 #include <string>
-#include "../model.h"
+#include <vector>
 
-class Building : public Model{
+class Building {
    private:
-    std::vector<Id> adj; //directly adjacent buildings
-    std::pair<double, double> coord; //coordinate
-    std::string campus_type; //which campus itself in
+    std::vector<int> adj;              // directly adjacent buildings
+    std::pair<double, double> coord;  // coordinate
+    std::string campus_type;          // which campus itself in
 
    public:
     Building() = default;
     Building(std::string);
 
     void set_coord();
-    std::pair<int,int> get_coord();
+    std::pair<int, int> get_coord();
 
     std::string get_campus_type();
     void set_campus_type();
 
     void add_adj(Building adj);
-    std::vector<int> & get_adj(Id building); //get the adjacent building
+    std::vector<int>& get_adj(int building);  // get the adjacent building
 };
 
 #endif  // NAVIGATION_BUILDING_H
