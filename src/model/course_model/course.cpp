42f32f0a86c4c9ff922708d7a3a80a5c5187d601
zhouyu <zhouyuguanshui@163.com>
2022-04-30 19:06:39 +0800

    Add course_model

--- src/model/course_model/course.cpp
+++ src/model/course_model/course.cpp
@@ -1,9 +1,9 @@
 #include "course.h"
 #include <fstream>
-Course::Course(string course_id, string course_name,string sdudent_id) {
+Course::Course(string course_id, string course_name,string id) {
     this->course_id=course_id;
     this->course_name=course_name;
-    this->student_id=sdudent_id;
+    this->user_id=id;
 }
 void Course::init(){
     string courese_filename="../../src/model/course_model/course_collection/"+course_id+"_course.txt";
@@ -19,10 +19,11 @@ void Course::init(){
     ifs>>times_per_week;
     for(int i=1;i<=times_per_week;i++){
       single_course a;
-      ifs>>a.date>>a.seq>>a.campus>>a.place;
+      ifs>>a.date>>a.seq>>a.campus>>a.place>>course_name;
       client_table.push_back(a);
     }
     ifs>>teacher_name>>teacher_id>>course_qun>>total_weeks;
+    ifs>>final.week>>final.date>>final.st_hour>>final.st_min>>final.fi_hour>>final.fi_min>>final.campus>>final.place;
     ifs>>ref_book_number;
     for(int i=1;i<=ref_book_number;i++){
       string book_name;
@@ -43,13 +44,14 @@ void Course::init(){
     string object_id;
     for(int i=1;i<=student_number;i++){
         ifs>>object_id;
-        if(object_id==student_id){
+        if(object_id==user_id){
              for(int i=1;i<=homework_number;i++){
                 ifs>>hw_table[i].second;              
                 }
         break;
         }
     }
+    ifs.close();
 }
 
 void Course::submit_homework(){
@@ -69,6 +71,27 @@ void Course::query_material_by_weight(){
 
 void Course::operMenu(){
     cout << endl;
+    cout<<"课程名称："<<course_name<<endl;
+    cout<<"每周课时数目："<<times_per_week<<endl;
+    for(int i=0;i<=times_per_week-1;i++){
+      single_course a=client_table[i];
+      int sh=course_start_time_table[a.seq]/60;
+      int sm=course_start_time_table[a.seq]%60;
+      int fh=course_finish_time_table[a.seq]/60;
+      int fm=course_finish_time_table[a.seq]%60;
+      cout<<a.date<<" "<<sh<<":"<<sm<<"-"<<fh<<":"<<fm<<" "<<a.campus<<a.place<<endl;
+    }
+    cout<<endl;
+    cout<<"任课教师："<<teacher_name<<endl;
+    cout<<"课程群："<<course_qun<<endl;
+    cout<<"当前进度："<<" "<<"/"<<total_weeks<<endl;
+    cout<<"考试时间："<<"第"<<final.week<<"周"<<final.date;
+    cout<<final.st_hour<<":"<<final.st_min<<"-"<<final.fi_hour<<":"<<final.fi_min<<endl;
+    cout<<"考试地点："<<final.campus<<final.place<<endl;
+    cout<<"课程参考书籍："<<endl;
+    for(int i=0;i<=ref_book_number-1;i++){
+     cout<<ref_books[i]<<endl;
+    }
     cout << "\t\t -----------------------\n";
     cout << "\t\t|                       |\n";
     cout << "\t\t|     1.提交课程作业    |\n";
--- src/model/course_model/course.h
+++ src/model/course_model/course.h
@@ -15,7 +15,8 @@ struct single_course{
 };
 struct exam{
     int week;
-    int start_hour,start_min,finish_hour,finish_min;
+    string date;
+    int st_hour,st_min,fi_hour,fi_min;
     string campus;
     string place;
 };
@@ -28,6 +29,8 @@ class Course {
     public:
     string course_id; //课程编号
     string course_name; //课程名称
+    int course_start_time_table[12]={0,8*60,8*60+50,9*60+50,10*60+40,11*60+30,13*60,13*60+50,14*60+45,15*60+40,16*60+40,17*60+35};
+    int course_finish_time_table[12]={0,8*60+45,9*60+35,10*60+35,11*60+25,12*60+15,13*60+45,14*60+35,15*60+30,16*60+25,17*60+25,18*60+10};
     int times_per_week,homework_number,material_number,ref_book_number,total_weeks,student_number;
     vector<string> ref_books;
     vector<single_course> client_table;
@@ -35,7 +38,7 @@ class Course {
     vector<material> materials;
     string teacher_name;
     string teacher_id;
-    string student_id;
+    string user_id;
     string course_qun;
     exam final;
     Course();
