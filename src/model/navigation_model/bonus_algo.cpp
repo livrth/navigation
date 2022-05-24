12059702d7c0a64e12038e49632b52dcadde546e
Mona Lisa <45538642+livrth@users.noreply.github.com>
2022-05-24 13:06:53 +0800

    Create new file for bonus algorithm

---
+++ src/model/navigation_model/bonus_algo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "guide.h"
+
+using namespace std;
+
+void Guide::print_path_by_fixed_building() {  //选做算法  经过固定地点
+    cout << "test" << endl;
+
+    system("pause");
+    system("cls");
+}
--- src/model/navigation_model/guide.cpp
+++ src/model/navigation_model/guide.cpp
@@ -1522,5 +1522,5 @@ void Guide::print_path_by_time() {
     system("cls");
 }
 
-void Guide::print_path_by_fixed_building() {  //选做算法  经过固定地点
-}
+// void Guide::print_path_by_fixed_building() {  //选做算法  经过固定地点
+// }
